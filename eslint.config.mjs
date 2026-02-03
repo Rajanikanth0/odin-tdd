182aab9ced236923e414b2869450fa7bab2fdfb7
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2026-02-03 17:18:48 +0530

    feat: add jest
    - add jest for tdd
    - to use esm instead of cjs(default in jest) add '@babel/preset-env'
    - add eslint-plugin-jest to prevent jest-globals error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,20 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 import eslintConfigPrettier from "eslint-config-prettier";
+import jestPlugin from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    files: ["**/*.test.{js,mjs,cjs}"],
+    plugins: { jest: jestPlugin },
+    rules: { ...jestPlugin.configs.recommended.rules },
+    languageOptions: { globals: globals.jest },
+  }, 
   eslintConfigPrettier
 ]);
